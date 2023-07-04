5f9968d5f2ba58a2461f707dec34a541910d3290
Ironraptor3 <dannyribaudo@gmail.com>
2023-07-04 18:23:11 -0400

    Pipe to null; win script testing

--- bin/win/clean_local.bat
+++ bin/win/clean_local.bat
@@ -4,7 +4,7 @@ PUSHD %~DP0\\..\\..\\
 SETLOCAL
 REM Use a var for consistency, still need to udpate .gitignore if this is changed
 set DOCS_OUT=local_docs
-RMDIR /S /Q %DOCS_OUT%
-DEL /Q %DOCS_OUT%.zip
+RMDIR /S /Q %DOCS_OUT% 2>NUL
+DEL /Q %DOCS_OUT%.zip 2>NUL
 ENDLOCAL
 POPD
