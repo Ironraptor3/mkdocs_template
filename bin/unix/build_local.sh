768257300edfa6bae9ae060215535250ab25c087
Ironraptor3 <dannyribaudo@gmail.com>
2023-07-04 16:54:45 -0400

    Zip functionality on build

--- bin/unix/build_local.sh
+++ bin/unix/build_local.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cd $(dirname $0)/../.. && mkdocs build --config-file local.yml "$@"
+
+# Use a var for consistency, still need to update .gitignore if this is changed
+DOCS_OUT="local_docs"
+cd $(dirname $0)/../.. && mkdocs build --config-file local.yml --site-dir $(DOCS_OUT) "$@" && zip "$(DOCS_OUT).zip" $(DOCS_OUT)
+unset DOCS_OUT
--- bin/win/build_local.bat
+++ bin/win/build_local.bat
@@ -1,5 +1,11 @@
 @echo off
 REM This may not be the safest way to perform this
 pushd %~dp0\\..\\..\\
-mkdocs build --config-file local.yml %*
+SETLOCAL
+REM Use a var for consistency, still need to update .gitignore if this is changed
+set docs_out=local_docs
+mkdocs build --config-file local.yml --site-dir %docs_out% %*
+tar.exe -a -cf %docs_out%.zip %docs_out%
+ENDLOCAL
 popd
+
